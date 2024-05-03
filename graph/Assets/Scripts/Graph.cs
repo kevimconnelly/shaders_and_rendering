71a53b43b1e736f7e384c4790c17da2325946648
Kevin <kevinconnelly1337@gmail.com>
2024-05-03 14:59:00 -0700

    add resolution to graph

--- graph/Assets/Scripts/Graph.cs
+++ graph/Assets/Scripts/Graph.cs
@@ -6,12 +6,21 @@ public class Graph : MonoBehaviour
     [SerializeField]
     Transform pointPrefab;
 
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
     void Awake ()
     {
-        for (int i = 0; i < 10; i++) {
-            
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
+
+        for (int i = 0; i < resolution; i++) {
             Transform point = Instantiate(pointPrefab);
-            point.localPosition = Vector3.right * i;
+            position.x = (i + 0.5f) * step - 1f;
+            position.y = position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
         }
     }
 }
