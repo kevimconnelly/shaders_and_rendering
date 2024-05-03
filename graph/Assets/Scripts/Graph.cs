05de5540bb5efa3034871609b459edef0ed9d37e
Kevin <kevinconnelly1337@gmail.com>
2024-05-03 15:27:37 -0700

    make point shader

--- graph/Assets/Scripts/Graph.cs
+++ graph/Assets/Scripts/Graph.cs
@@ -21,6 +21,7 @@ public class Graph : MonoBehaviour
             position.y = position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform, false);
         }
     }
 }
