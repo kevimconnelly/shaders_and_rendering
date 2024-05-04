3bdd1db21b94d4f945c7e500098c7db4d06e04f8
Kevin <kevinconnelly1337@gmail.com>
2024-05-04 09:55:55 -0700

    add ripple function

--- graph/Assets/Scripts/FunctionLibrary.cs
+++ graph/Assets/Scripts/FunctionLibrary.cs
@@ -9,8 +9,15 @@ public static class FunctionLibrary {
 
     public static float MultiWave(float x, float t)
     {
-        float y = Sin(PI * (x + t));
+        float y = Sin(PI * (x + 0.5f * t));
         y += 0.5f * Sin(2f * PI * (x + t));
         return y * (2f / 3f);
+    }
+
+    public static float Ripple(float x, float t)
+    {
+        float d = Abs(x);
+        float y = Sin(PI * (4f * d - t));
+        return y / (1f + 10f * d);
     }
 }
