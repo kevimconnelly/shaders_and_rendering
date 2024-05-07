91f69077962b990b5fe9e94559a118db22ba289d
Kevin <kevinconnelly1337@gmail.com>
2024-05-07 16:48:32 -0700

    fix infinite loop

--- graph/Assets/Scripts/FunctionLibrary.cs
+++ graph/Assets/Scripts/FunctionLibrary.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 using static UnityEngine.Mathf;
 
 public static class FunctionLibrary {
-    public delegate float Function(float x, float z, float t);
+    public delegate Vector3 Function(float u, float v, float t);
 
-    public enum FunctionName { Wave, MultiWave, Ripple }
+    public enum FunctionName { Wave, MultiWave, Ripple };
 
     static Function[] functions = { Wave, MultiWave, Ripple };
 
@@ -14,22 +14,34 @@ public static class FunctionLibrary {
         return functions[(int)name];
     }
 
-    public static float Wave(float x, float z, float t) {
-        return Mathf.Sin(Mathf.PI * (x + z + t));
+    public static Vector3 Wave(float u, float v, float t) {
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + v + t));
+        p.z = v;
+        return p;
     }
 
-    public static float MultiWave(float x, float z, float t)
+    public static Vector3 MultiWave(float u, float v, float t)
     {
-        float y = Sin(PI * (x + 0.5f * t));
-        y += 0.5f * Sin(2f * PI * (z + t));
-        y += Sin(PI * (x + z + 0.25f * t));
-        return y * (1f / 2.5f);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + 0.5f * t));
+        p.y += 0.5f * Sin(2f * PI * (v + t));
+        p.y += Sin(PI * (u + v + 0.25f * t));
+        p.y *= 1f / 2.5f;
+        p.z = v;
+        return p;
     }
 
-    public static float Ripple(float x, float z, float t)
+    public static Vector3 Ripple(float u, float v, float t)
     {
-        float d = Sqrt(x * x + z * z);
-        float y = Sin(PI * (4f * d - t));
-        return y / (1f + 10f * d);
+        float d = Sqrt(u * u + v * v);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (4f * d - t));
+        p.y /= 1f + 10f * d;
+        p.z = v;
+        return p;
     }
 }
