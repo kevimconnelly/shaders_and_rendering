36b60282e0bbb12a77fd11a5f31fb1fda62e4e94
Kevin <kevinconnelly1337@gmail.com>
2024-05-09 09:59:02 -0700

    render cube from gpu

--- graph/Assets/Scripts/GPUGraph.cs
+++ graph/Assets/Scripts/GPUGraph.cs
@@ -11,6 +11,12 @@ public class GPUGraph : MonoBehaviour
     [SerializeField]
     ComputeShader computeShader;
 
+    [SerializeField]
+    Material material;
+
+    [SerializeField]
+    Mesh mesh;
+
     static readonly int
         positionsId = Shader.PropertyToID("_Positions"),
         resolutionId = Shader.PropertyToID("_Resolution"),
@@ -43,6 +49,9 @@ public class GPUGraph : MonoBehaviour
         computeShader.SetBuffer(0, positionsId, positionsBuffer);
         int groups = Mathf.CeilToInt(resolution / 8f);
         computeShader.Dispatch(0, groups, groups, 1);
+
+        var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
+        Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, positionsBuffer.count);
     }
 
     void OnEnable()
