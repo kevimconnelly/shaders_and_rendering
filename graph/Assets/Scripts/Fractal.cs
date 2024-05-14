83c81ca14887d43010ff87341ecbf93ada29802f
Kevin <kevinconnelly1337@gmail.com>
2024-05-13 17:15:41 -0700

    send compute shader matrices to gpu

--- graph/Assets/Scripts/Fractal.cs
+++ graph/Assets/Scripts/Fractal.cs
@@ -38,14 +38,19 @@ public class Fractal : MonoBehaviour
         rotation = rotations[childIndex]
     };
 
-    void Awake()
+    ComputeBuffer[] matricesBuffers;
+
+    void OnEnable()
     {
         parts = new FractalPart[depth][];
         matrices = new Matrix4x4[depth][];
+        matricesBuffers = new ComputeBuffer[depth];
+        int stride = 16 * 4;
         for (int i = 0, length = 1; i < parts.Length; i++, length *= 5)
         {
             parts[i] = new FractalPart[length];
             matrices[i] = new Matrix4x4[length];
+            matricesBuffers[i] = new ComputeBuffer(length, stride);
         }
 
         parts[0][0] = CreatePart(0);
@@ -59,6 +64,26 @@ public class Fractal : MonoBehaviour
                     levelParts[fpi + ci] = CreatePart(ci);
                 }
             
+        }
+    }
+
+    void OnDisable()
+    {
+        for (int i = 0; i < matricesBuffers.Length; i++)
+        {
+            matricesBuffers[i].Release();
+        }
+        parts = null;
+        matrices = null;
+        matricesBuffers = null;
+    }
+
+    void OnValidate()
+    {
+        if (parts != null && enabled)
+        {
+            OnDisable();
+            OnEnable();
         }
     }
 
@@ -99,6 +124,11 @@ public class Fractal : MonoBehaviour
                     part.worldPosition, part.worldRotation, scale * Vector3.one
                 );
             }
+        }
+
+        for (int i = 0; i < matricesBuffers.Length; i++)
+        {
+            matricesBuffers[i].SetData(matrices[i]);
         }
     }
 }
