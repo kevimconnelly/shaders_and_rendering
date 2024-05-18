72ae577e06a2edd914b33a27e4479169fe71fb2c
Kevin <kevinconnelly1337@gmail.com>
2024-05-17 20:42:52 -0700

    add multiple color blending

--- graph/Assets/Scripts/Fractal.cs
+++ graph/Assets/Scripts/Fractal.cs
@@ -47,9 +47,10 @@ public class Fractal : MonoBehaviour {
 		public float spinAngle;
 	}
 
-	static readonly int 
-		baseColorId = Shader.PropertyToID("_BaseColor"),
-		matricesId = Shader.PropertyToID("_Matrices"),
+	static readonly int
+        colorAId = Shader.PropertyToID("_ColorA"),
+        colorBId = Shader.PropertyToID("_ColorB"),
+        matricesId = Shader.PropertyToID("_Matrices"),
         sequenceNumbersId = Shader.PropertyToID("_SequenceNumbers");
 
     static float3[] directions = {
@@ -74,7 +75,7 @@ public class Fractal : MonoBehaviour {
 	Material material;
 
     [SerializeField]
-    Gradient gradient;
+    Gradient gradientA, gradientB;
 
     NativeArray<FractalPart>[] parts;
 
@@ -165,9 +166,9 @@ public class Fractal : MonoBehaviour {
 		for (int i = 0; i < matricesBuffers.Length; i++) {
 			ComputeBuffer buffer = matricesBuffers[i];
 			buffer.SetData(matrices[i]);
-            propertyBlock.SetColor(
-                baseColorId, gradient.Evaluate(i / (matricesBuffers.Length - 1f))
-            );
+            float gradientInterpolator = i / (matricesBuffers.Length - 1f);
+            propertyBlock.SetColor(colorAId, gradientA.Evaluate(gradientInterpolator));
+            propertyBlock.SetColor(colorBId, gradientB.Evaluate(gradientInterpolator));
             propertyBlock.SetBuffer(matricesId, buffer);
             propertyBlock.SetVector(sequenceNumbersId, sequenceNumbers[i]);
             Graphics.DrawMeshInstancedProcedural(
